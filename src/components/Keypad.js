eb5dec8d68e8f483b6efe909163a342a72869afd
Robert Haji <roberthadji66@gmail.com>
2025-04-28 22:18:12 +0300

    lab-done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+     const handleChange = (e) => {
+       console.log("Entering password...");
+     };
+
     return (
-        <div></div>
-    )
+      <div>
+        <input
+          type="password"
+          onChange={(e) => {
+            e.target.value;
+            console.log("Entering password...");
+          }}
+        />
+        <input type="password" onChange={handleChange} />
+      </div>
+    );
+
+    
 }
 
 export default Keypad;
